5d043a626cdd90b6d5ae155bd723abc8e34de8f1
Ethan Favia <Ethan Favia>
2016-06-14 15:21:09 -0400

    game logic done now styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.questions').hide();
-	$('#info').hide();
+	$('.info').hide();
 	$('#restart').hide();
 	var correct = 0;
 	var wrong = 0;
@@ -24,8 +24,27 @@ $(document).ready(function(){
 		q3= {question: 'Half of all americans live within 50 miles of what?',
 			right: 'their birthplace',
 			choices:['an airport', 'a sports stadium', 'their birthplace', 'their favorite vacation spot'],
-
-		}
+		},
+		q4= {question: 'How many different shapes do animal crackers come in?',
+			right: '18',
+			choices:['8', '18', '12', '40'],
+		},
+		q5= {question: 'Dragonflys have a lifespan of how long?',
+			right: '24 hours',
+			choices:['24 hours', 'a week', 'three days', 'one month'],
+		},
+		q6= {question: 'In 1796 there was a state called Franklin, that state is now known as?',
+			right: 'Tennessee',
+			choices:['New Hampshire', 'Virginia', 'Tennessee', 'Pennsylvania'],
+		},
+		q7= {question: 'What do 100% of lottery winners do?',
+			right: 'Gain weight',
+			choices:['End up happy', 'Donate to charity', 'Go broke', 'Gain weight'],
+		},
+		q8= {question: 'What do 100% of lottery winners do?',
+			right: 'Gain weight',
+			choices:['End up happy', 'Donate to charity', 'Go broke', 'Gain weight'],
+		},
 	]
 	 var clock = {
 		time: 20,
@@ -33,11 +52,11 @@ $(document).ready(function(){
 		int2:0,
 		reset:function() {
 			clock.time = 20; 
-			$('#timerem').html(clock.time);
+			$('#timerem').html("time remaing: " + clock.time);
 		},
 		count: function() {
 			clock.time--;
-			$('#timerem').html(clock.time);
+			$('#timerem').html("time remaing: " + clock.time);
 		},
 		start:function() {
 			int1 = setInterval(clock.count, 1000);
@@ -49,7 +68,7 @@ $(document).ready(function(){
 		},
 		out: function() {
 			clock.stop();
-			$('#info').show();
+			$('.info').show();
 			$('#info').html('wrong! the correct answer was ' + currentQ.right + '!');
 			$('.questions').hide();
 			wrong++;
@@ -66,14 +85,14 @@ $(document).ready(function(){
 		if(r < questions.length){
 			clock.reset();
 			clock.start();
-			$('#info').hide();
+			$('.info').hide();
 			$('.questions').show();
 			currentQ = questions[r];
 			console.log(currentQ);
 			$('#question').html(currentQ.question);
 				for(i=0; i<currentQ.choices.length; i++){
-					$('#choice'+ parseInt(i+1)).html( currentQ.choices[i]);
 					$('#'+ parseInt(i+1)).attr('value', currentQ.choices[i]);
+					$('#'+ parseInt(i+1)).html(currentQ.choices[i]);
 			}
 		r++; 
 
@@ -103,14 +122,14 @@ $(document).ready(function(){
 		guess = $(this).attr("value"); 
 		console.log($(this).attr("value"));
 		if($(this).attr("value") == currentQ.right){
-			$('#info').show();
+			$('.info').show();
 			$('#info').html('correct!')
 			$('.questions').hide();;
 			clock.stop();
 			setTimeout(setQuestion, 3000);
 			correct++;
 		}  else {
-			$('#info').show();
+			$('.info').show();
 			$('#info').html('wrong! the correct answer was ' + currentQ.right + '!');
 			$('.questions').hide();
 			clock.stop();
